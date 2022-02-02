c384f549269395bc7d2f16430cba004d97873914
Mathew Jacob <mathewjacob@Mathews-MacBook-Pro.local>
2022-02-02 17:19:27 +0530

    Practicing Component

---
+++ practicing-components/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { warningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    warningAlertComponent,
+    SuccessAlertComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
